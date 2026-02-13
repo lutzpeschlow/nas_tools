724831cdeb924a23c2982e09a3d9c965e0d12825
lutzpeschlow <lutz.peschlow@gmail.com>
2026-02-13 17:51:10 +0100

    added - valid json ctrl file - test

--- ctrl/ctrl_test.go
+++ ctrl/ctrl_test.go
@@ -7,6 +7,9 @@ import (
 	"github.com/lutzpeschlow/nas_tools/objects"
 )
 
+// TEST
+//
+// valid json file
 func TestReadControlJsonFile_Success(t *testing.T) {
 	ctrlObj := objects.Control{}
 
@@ -16,37 +19,49 @@ func TestReadControlJsonFile_Success(t *testing.T) {
 	}
 
 	// Prüfe Ergebnisse
-	if ctrlObj.Action != "myaction" {
+	if ctrlObj.Action != "READ" {
 		t.Errorf("Expected action 'myaction', got '%s'", ctrlObj.Action)
 	}
-	if ctrlObj.InputFile != "test.nas" {
-		t.Errorf("Expected InputFile 'test.nas', got '%s'", ctrlObj.InputFile)
+	if ctrlObj.InputFile != "reg_test_01.dat" {
+		t.Errorf("Expected InputFile 'reg_test_01.dat', got '%s'", ctrlObj.InputFile)
 	}
-	if ctrlObj.FullInputPath != "input/test.nas" {
-		t.Errorf("Expected FullInputPath 'input/test.nas', got '%s'", ctrlObj.FullInputPath)
+	if ctrlObj.OutputFile != "result.txt" {
+		t.Errorf("Expected OutputFile 'result.txt', got '%s'", ctrlObj.OutputFile)
 	}
 }
 
+// TEST
+//
+//	error should output in case of non-existent json file
 func TestReadControlJsonFile_MissingFile(t *testing.T) {
+	// instance of object
 	ctrlObj := objects.Control{}
-
+	// try to read a non-existent json file
 	err := ReadControlJsonFile("testdata/nonexistent.json", &ctrlObj, "linux")
+	// in case of success, direct fatal
 	if err == nil {
 		t.Fatal("Expected error for missing file")
 	}
+	// check for PASS
+	//    error contains string - read control file
 	if !strings.Contains(err.Error(), "read control file") {
 		t.Errorf("Expected file read error, got: %v", err)
 	}
 }
 
+// TEST
+//
+//	invalid action in json file
 func TestReadControlJsonFile_InvalidAction(t *testing.T) {
-	// testdata/control_invalid.json ohne "myaction"
+	// instance of object
 	ctrlObj := objects.Control{}
-
+	// read invalid json fil
 	err := ReadControlJsonFile("testdata/control_invalid.json", &ctrlObj, "linux")
+	// error should be exist, if nil go out with fatal
 	if err == nil {
 		t.Fatal("Expected action not found error")
 	}
+	// no action found, error message should contain string ...
 	if !strings.Contains(err.Error(), "action myaction not found") {
 		t.Errorf("Expected action error, got: %v", err)
 	}
